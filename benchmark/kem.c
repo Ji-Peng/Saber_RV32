e74b7718f23f26150c5269a1aed2a1fb941e5594
Ji-Peng WFL <jp-zhang@outlook.com>
2021-01-05 21:13:37 +0800

    add kex test

--- benchmark/kem.c
+++ benchmark/kem.c
@@ -47,9 +47,47 @@ static int test_kem_cca(void)
     return 0;
 }
 
+static int test_kem_cpa(void)
+{
+    uint8_t pk[CRYPTO_PUBLICKEYBYTES];
+    uint8_t sk[CRYPTO_SECRETKEYBYTES];
+    uint8_t ct[CRYPTO_CIPHERTEXTBYTES];
+    uint8_t message1[64], message2[64];
+    uint8_t noiseseed[32];
+    uint8_t entropy_input[48];
+    int i;
+
+    for (i = 0; i < 48; i++)
+        entropy_input[i] = i;
+    randombytes_init(entropy_input, NULL);
+
+    memset(message1, 0, sizeof(message1));
+    memset(message2, 0, sizeof(message2));
+    memset(noiseseed, 0, sizeof(noiseseed));
+
+    for (i = 0; i < 32; i++) {
+        message1[i] = noiseseed[i] = i;
+    }
+
+    indcpa_kem_keypair(pk, sk);
+    indcpa_kem_enc(message1, noiseseed, pk, ct);
+    indcpa_kem_dec(sk, ct, message2);
+
+    for (i = 0; i < 64; i++) {
+        if (message1[i] != message2[i]) {
+            printf("i=%d, %d, %d\n", i, message1[i], message2[i]);
+            printf("ERROR\n");
+            break;
+        }
+    }
+    printf("the end\n");
+    return 0;
+}
+
 int main(void)
 {
     printf("hello world\n");
+    test_kem_cpa();
     test_kem_cca();
     return 0;
 }
