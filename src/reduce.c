07bf2b152960aba7a8ae4ea950cc81de7c9050d5
jipengASC <jp-zhang@outlook.com>
2021-03-02 16:42:56 +0800

    barrett_reduce_2q is not ok

--- src/reduce.c
+++ src/reduce.c
@@ -42,18 +42,32 @@ int32_t barrett_reduce(int32_t a)
     return a - t;
 }
 
-// int main(void)
-// {
-//     printf("%d\n", montgomery_reduce((int64_t)M * RmodM));
-//     printf("%d\n", montgomery_reduce((int64_t)(M - 1) * RmodM));
-//     printf("%d\n", montgomery_reduce((int64_t)(2 * M) * RmodM));
-//     printf("%d\n", montgomery_reduce((int64_t)(2 * M - 1) * RmodM));
-//     printf("%d\n", montgomery_reduce((int64_t)(3 * M) * RmodM));
-//     printf("%d\n", montgomery_reduce((int64_t)(3 * M - 1) * RmodM));
-//     printf("%d\n", barrett_reduce(M));
-//     printf("%d\n", barrett_reduce(M - 1));
-//     printf("%d\n", barrett_reduce(2 * M));
-//     printf("%d\n", barrett_reduce(2 * M - 1));
+int32_t barrett_reduce_2q(int64_t a)
+{
+    int32_t t;
+    // log2(v)=30.996
+    const int32_t v = (((int64_t)1 << 53) + M / 2) / M;
+    t = ((int64_t)v * (int32_t)(a >> 21)) >> 32;
+    t *= M;
+    return a - t;
+}
 
-//     printf("%d\n", barrett_reduce(M/2+3));
-// }
+int main(void)
+{
+    printf("%d\n", montgomery_reduce((int64_t)M * RmodM));
+    // printf("%d\n", montgomery_reduce((int64_t)(M - 1) * RmodM));
+    // printf("%d\n", montgomery_reduce((int64_t)(2 * M) * RmodM));
+    // printf("%d\n", montgomery_reduce((int64_t)(2 * M - 1) * RmodM));
+    // printf("%d\n", montgomery_reduce((int64_t)(3 * M) * RmodM));
+    // printf("%d\n", montgomery_reduce((int64_t)(3 * M - 1) * RmodM));
+    printf("%d\n", barrett_reduce(M));
+    // printf("%d\n", barrett_reduce(M - 1));
+    // printf("%d\n", barrett_reduce(2 * M));
+    // printf("%d\n", barrett_reduce(2 * M - 1));
+    // printf("%d\n", barrett_reduce(M/2+3));
+    printf("%d\n", barrett_reduce_2q(M));
+    printf("%d\n", barrett_reduce_2q(2 * M));
+    printf("%d\n", barrett_reduce_2q(2 * M + 1));
+    printf("%d\n", barrett_reduce_2q(23 * M + 1));
+    printf("%d\n", barrett_reduce_2q((int64_t)M * M - 1));
+}
