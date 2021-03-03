295e7694ce2e4c6ecd920ff799e3e79267697265
jipengASC <jp-zhang@outlook.com>
2021-03-03 10:29:36 +0800

    clean up ntt and ntests speed

--- src/ntt.c
+++ src/ntt.c
@@ -101,8 +101,7 @@ void invntt(int32_t in[256], int32_t out[256])
         zeta = inv_root_table[k++];
         for (j = start; j < start + len; j++) {
             t = in[j];
-            out[j] = barrett_reduce(t + in[j + len]);
-            // out[j] = t + in[j + len];
+            out[j] = t + in[j + len];
             out[j + len] = t - in[j + len];
             out[j + len] = fqmul(zeta, out[j + len]);
         }
@@ -113,8 +112,7 @@ void invntt(int32_t in[256], int32_t out[256])
             zeta = inv_root_table[k++];
             for (j = start; j < start + len; j++) {
                 t = out[j];
-                out[j] = barrett_reduce(t + out[j + len]);
-                // out[j] = t + out[j + len];
+                out[j] = t + out[j + len];
                 out[j + len] = t - out[j + len];
                 out[j + len] = fqmul(zeta, out[j + len]);
             }
@@ -125,7 +123,6 @@ void invntt(int32_t in[256], int32_t out[256])
     for (j = 0; j < 256; j++) {
         out[j] = fqmul(out[j], f);
         out[j] = barrett_reduce(out[j]);
-        out[j] &= 0x1fff;
     }
 }
 
