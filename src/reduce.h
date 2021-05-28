4a5fdc3de0064f6e6bb8bd5c49beabf2e5f1125c
Jipeng Zhang <jp-zhang@outlook.com>
2021-05-28 05:13:28 -0700

    fast montgomery reduction

--- src/reduce.h
+++ src/reduce.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 #define M 10487809          // M = 512 * 20484 + 1 < 2^24
-#define Mprime -4288673793  // M * Mprime = -1 mod R (R=2^32)
+#define Mprime 4288673793  // M * Mprime = 1 mod R (R=2^32)
 #define MINV 4288673793     // M * MINV = 1 mod R
 #define RmodM 5453415       // R mod M
 #define NINV 2466627        // R^2 * (1/64) mod M
